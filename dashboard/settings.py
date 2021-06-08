525c8947a54f61b2356c6bc918b592253ea88ab7
ikechukwueze <themedric@live.com>
2021-06-08 09:21:02 +0100

    changed allowed hosts

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zcqpz&y%dj&j-$k$=o)5p-h+!o1*f6cmd&@f1voz23q!(#zc-d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://stockpricedashboard.azurewebsites.net']
+ALLOWED_HOSTS = ['https://stockpricedashboard.azurewebsites.net', 'http://stockpricedashboard.azurewebsites.net']
 
 
 # Application definition
